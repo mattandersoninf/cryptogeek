9c1ad81466cf297b5999e5ad87f9d84b52476ef1
Matthew Anderson <matt.anderson.inf@gmail.com>
2019-04-26 00:45:05 -0400

    Created a python script to provide the structure for a class file that will be the base for the web scraper. Switched to using Selenium as the webscraping library since it can dig into dynamic webpages whereas bs4 and requests can grab static pages

---
+++ scrape.py
@@ -0,0 +1,69 @@
+# Main scraping class file:
+
+# requests and beautifulsoup4 are ideal for grabbing data from static webpages
+import requests
+from bs4 import BeautifulSoup as soup
+
+# import csv
+
+# for printing to analyze
+from pprint import pprint
+
+# web automation library
+from selenium import webdriver
+
+
+cryptoCompareUrl = 'https://www.cryptocompare.com/'
+
+"""
+class mainScraper(self,siteList):
+    
+    for site in siteList:
+        
+"""
+
+
+"""
+def getWebpage(url):
+    
+    # get google Chrome webdriver
+    browser = webdriver.Chrome()
+    browser.get(url)
+"""    
+    
+
+def getCryptoCompareSoup():
+    
+    # get google Chrome webdriver
+    browser = webdriver.Chrome()
+    browser.get(cryptoCompareUrl)
+    
+    # request the cryptocompare webpage
+    cryptoCompareHtml = browser.execute_script("return document.documentElement.innerHTML;") 
+    
+    """
+    # requests.get(cryptoCompareUrl)
+    
+    #cryptoCompareHtml = browser.page_source
+    
+    #BeautifulSoup(cryptoCompareRequest, '')
+    """
+    
+    # close chrome browser that you opened with selenium
+    browser.close()
+    
+    # return the innerhtml of the web page
+    return cryptoCompareHtml
+
+
+
+
+# function to scrape cryptocurrency information from  https://www.cryptocompare.com/
+#def scrapeCryptoCompare():
+    
+    #form the 
+
+
+if __name__=="__main__":
+    
+    pprint(getCryptoCompareSoup())
